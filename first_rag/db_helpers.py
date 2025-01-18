4b002d65b3022945431b209537e1b82fb670d045
Aung Khant Phyo <itsakphyo@gmail.com>
2025-01-18 13:05:27 +0700

    ...

--- first_rag/db_helpers.py
+++ first_rag/db_helpers.py
@@ -51,29 +51,3 @@ def generate_data_store(data_path, chunk_size, chunk_overlap, chroma_path):
     chunks = split_text(documents, chunk_size, chunk_overlap)
     save_to_chroma(chunks, chroma_path)
 
-
-# def get_yt_video_transcribed(url):
-#     yt = pt.YouTube(url)
-#     yt.streams.filter(only_audio=True).first().download(filename="yt_audio.mp3", output_path=path)
-#     model = whisper.load_model("base")
-#     transcribed_text = model.transcribe(path)
-
-
-
-
-# import pytube as pt
-# import whisper
-
-# def download_video(url, path):
-#     yt = pt.YouTube(url)
-#     yt.streams.filter(progressive=True, file_extension='mp4').first().download(filename="yt_video.mp4", output_path=path)
-
-# def download_audio(url, path):
-#     yt = pt.YouTube(url)
-#     yt.streams.filter(only_audio=True).first().download(filename="yt_audio.mp3", output_path=path)
-
-
-# path = "yt_audio.mp3"
-
-# model = whisper.load_model("base")
-# transcribed_text = model.transcribe(path)
